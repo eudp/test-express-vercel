8e1656d44a886d9f3bc2186e4285ec62bf2df7ed
efrainpubgenius <efraub@pubgenius.io>
2022-05-16 16:41:13 -0400

    push

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 const { Client } = require("pg");
 const client = new Client({
   user: "postgres",
